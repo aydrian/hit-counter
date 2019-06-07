1d534dadaa1de3376c81f4f0563fd132a29a74c8
Aydrian Howard <aydrian@gmail.com>
2019-06-07 13:16:53 -0400

    Removed location info and added reach router

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,5 +1,8 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+
+const domain = "aydrian.moolumi.io"
 
 // Create an AWS resource (DynamoDB Table)
 const pageViews = new aws.dynamodb.Table('page-views', {
@@ -12,6 +15,13 @@ const pageViews = new aws.dynamodb.Table('page-views', {
 
 // Create an Internet-facing HTTP API
 const endpoint = new awsx.apigateway.API("hit-counter", {
+  stageName: pulumi.getStack(),
+  staticRoutesBucket: {
+    website: {
+      indexDocument: "index.html",
+      errorDocument: "index.html"
+    }
+  },
   routes: [
     {
       path: '/',
@@ -46,41 +56,29 @@ const endpoint = new awsx.apigateway.API("hit-counter", {
       method: 'POST',
       eventHandler: async event => {
         const { headers: { "User-Agent": userAgent}, requestContext: {requestTime, requestId, identity: { sourceIp } }} = event;
-        const body = event.body || ''
-        const payload = JSON.parse(Buffer.from(body, 'base64').toString())
-        const { lat=0, long=0 } = payload;
         try {
-        const client = new aws.sdk.DynamoDB.DocumentClient()
-        // Write new hit
-        await client.put({
-          TableName: pageViews.name.get(),
-          Item: {
-            id: requestId,
-            userAgent,
-            requestTime,
-            sourceIp,
-            location: {
-              type: "Point",
-              coordinates: [lat, long]
+          const client = new aws.sdk.DynamoDB.DocumentClient()
+          // Write new hit
+          await client.put({
+            TableName: pageViews.name.get(),
+            Item: {
+              id: requestId,
+              userAgent,
+              requestTime,
+              sourceIp
             }
+          }).promise()
+          return {
+            statusCode: 201,
+            body: ""
           }
-        }).promise()
-        // Get hits to return
-        const scanResult = await client.scan({TableName: pageViews.name.get()}).promise()
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            items: scanResult.Items,
-            count: scanResult.Count
-          })
+        } catch (err) {
+          console.log(`POST /hit-me error: $err.stack}`)
+          return {
+            statusCode: 500,
+            body: 'An error occurred, please check the Pulumi Logs'
+          }
         }
-      } catch (err) {
-        console.log(`POST /hit-me error: $err.stack}`)
-        return {
-          statusCode: 500,
-          body: 'An error occurred, please check the Pulumi Logs'
-        }
-      }
       }
     }
   ]
--- src/App.js
+++ src/App.js
@@ -1,71 +1,80 @@
 import React, { Component } from "react";
+import { Router, Link } from "@reach/router";
 import axios from "axios";
 
-import { Container, Statistic, Table } from "semantic-ui-react";
+import { Container, Menu, Statistic, Table } from "semantic-ui-react";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lat: 0,
-      long: 0,
       views: [],
       count: 0
     };
   }
 
   componentDidMount = async () => {
-    axios.get(`${process.env.REACT_APP_STAGE}/views`).then(result => {
-      const { items: views, count } = result.data;
-      this.setState({ views, count });
-    });
-
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude: lat, longitude: long } = position.coords;
-      axios
-        .post(`${process.env.REACT_APP_STAGE}/hit-me`, { lat, long })
-        .then(result => {
-          const { items: views, count } = result.data;
-          this.setState({ views, lat, long, count });
-        })
-        .catch(err => console.log);
-    });
+    axios
+      .post(`${process.env.REACT_APP_STAGE}/hit-me`)
+      .then(() => {
+        return axios.get(`${process.env.REACT_APP_STAGE}/views`);
+      })
+      .then(result => {
+        const { items: views, count } = result.data;
+        this.setState({ views, count });
+      })
+      .catch(err => console.log);
   };
 
   render() {
+    const { views, count } = this.state;
     return (
-      <Container textAlign="center">
-        <Statistic label="Views" value={this.state.count} />
-        <Table celled striped>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Date</Table.HeaderCell>
-              <Table.HeaderCell>IP</Table.HeaderCell>
-              <Table.HeaderCell>User Agent</Table.HeaderCell>
-              <Table.HeaderCell>Location</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.views.map(view => {
-              return (
-                <Table.Row key={view.id}>
-                  <Table.Cell>{view.requestTime}</Table.Cell>
-                  <Table.Cell>{view.sourceIp}</Table.Cell>
-                  <Table.Cell>{view.userAgent}</Table.Cell>
-                  <Table.Cell>
-                    [{view.location.coordinates[0]},{" "}
-                    {view.location.coordinates[0]}]
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-        Current location: {this.state.lat}, {this.state.long}
+      <Container textAlign="center" style={{ marginTop: "1em" }}>
+        <Menu>
+          <Menu.Item header>Hit Counter</Menu.Item>
+          <Menu.Item>
+            <Link to={`${process.env.REACT_APP_STAGE}/`}>Home</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to={`${process.env.REACT_APP_STAGE}/details`}>Details</Link>
+          </Menu.Item>
+        </Menu>
+        <Router>
+          <Home path={`${process.env.REACT_APP_STAGE}/`} count={count} />
+          <Details
+            path={`${process.env.REACT_APP_STAGE}/details`}
+            views={views}
+          />
+        </Router>
       </Container>
     );
   }
 }
+
+const Home = ({ count }) => <Statistic label="Views" value={count} />;
+
+const Details = ({ views }) => (
+  <Table celled striped>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Date</Table.HeaderCell>
+        <Table.HeaderCell>IP</Table.HeaderCell>
+        <Table.HeaderCell>User Agent</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {views.map(({ id, requestTime, sourceIp, userAgent }) => {
+        return (
+          <Table.Row key={id}>
+            <Table.Cell>{requestTime}</Table.Cell>
+            <Table.Cell>{sourceIp}</Table.Cell>
+            <Table.Cell>{userAgent}</Table.Cell>
+          </Table.Row>
+        );
+      })}
+    </Table.Body>
+  </Table>
+);
 
 export default App;
